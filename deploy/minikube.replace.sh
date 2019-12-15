c7caabc4b544140903a104adb4226a7e4164696c
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2019-12-15 19:35:54 +0100

    Fix missing operator role metrics rule

---
+++ deploy/minikube.replace.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd ../ || exit
+minikube status
+
+eval $(minikube docker-env)
+operator-sdk build docker.io/wingsofovnia/metrics-webhook
+eval $(minikube docker-env --unset)
+
+kubectl replace --force -f deploy/service_account.yaml
+kubectl replace --force -f deploy/role.yaml
+kubectl replace --force -f deploy/role_binding.yaml
+
+kubectl replace --force -f deploy/crds/metrics.wingsofovnia.github.com_metricwebhooks_crd.yaml
+kubectl replace --force -f deploy/operator.yaml
+
+cd "$OLDPWD" || exit
