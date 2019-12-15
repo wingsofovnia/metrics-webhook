42adc7e694fa39981e3a97e7162a14b30b144855
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2019-12-15 15:48:07 +0100

    Define MetricWebhook CRD attributes

--- pkg/apis/metrics/v1alpha1/metricwebhook_types.go
+++ pkg/apis/metrics/v1alpha1/metricwebhook_types.go
@@ -1,26 +1,178 @@
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // MetricWebhookSpec defines the desired state of MetricWebhook
 // +k8s:openapi-gen=true
 type MetricWebhookSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// targetRef points to the target resource used to fetch pods
+	// for which metrics should be collected
+	TargetRef TargetRef `json:"targetRef"`
+	// webhook points to the web endpoint that going to get metric alerts
+	Webhook Webhook `json:"webhook"`
+	// metrics contains the specifications for metrics thresholds
+	// used to trigger webhook
+	// +listType=set
+	Metrics []MetricSpec `json:"metrics"`
+	// scrapeInterval defines how frequently to scrape metrics
+	ScrapeInterval metav1.Duration `json:"scrapeInterval"`
+}
+
+// TargetRef points to deployment or pod to watch metrics for
+// +k8s:openapi-gen=true
+type TargetRef struct {
+	// Kind of the referent; More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds"
+	Kind string `json:"kind"`
+	// Name of the referent; More info: http://kubernetes.io/docs/user-guide/identifiers#names
+	Name string `json:"name"`
+}
+
+// Webhook describes the web endpoint that the operator calls on metrics reaching their thresholds
+// +k8s:openapi-gen=true
+type Webhook struct {
+	// Kind of the referent service
+	Service string `json:"service"`
+	// Service port the webserver serves on
+	Port string `json:"port"`
+	// URL path to the webhook
+	Path string `json:"path"`
+}
+
+// +k8s:openapi-gen=true
+// MetricSourceType indicates the type of metric.
+type MetricSourceType string
+
+const (
+	// PodsMetricSourceType is a metric describing each pod in the current
+	// target (for example, transactions-processed-per-second).  The values
+	// will be averaged together before being compared to the target value.
+	PodsMetricSourceType MetricSourceType = "Pods"
+	// ResourceMetricSourceType is a resource metric known to Kubernetes, as
+	// specified in requests and limits, describing each pod in the current
+	// target (e.g. CPU or memory).
+	ResourceMetricSourceType MetricSourceType = "Resource"
+)
+
+// MetricSpec specifies metrics thresholds before webhook gets called
+// +k8s:openapi-gen=true
+type MetricSpec struct {
+	// type is the type of metric source.  It should be "Pods" or "Resource",
+	// each mapping to a matching field in the object.
+	Type MetricSourceType `json:"type"`
+
+	// pods refers to a metric describing each pod in the current scale target
+	// (for example, transactions-processed-per-second). The values will be
+	// averaged together before being compared to the target value.
+	// +optional
+	Pods *PodsMetricSource `json:"pods,omitempty"`
+	// resource refers to a resource metric (such as those specified in
+	// requests and limits) known to Kubernetes describing each pod in the
+	// current target (e.g. CPU or memory).
+	// +optional
+	Resource *ResourceMetricSource `json:"resource,omitempty"`
+}
+
+// PodsMetricSource indicates when to call webhook on a metric describing each pod in
+// the current target (for example, transactions-processed-per-second).
+// The values will be averaged together before being compared to the target
+// value.
+// +k8s:openapi-gen=true
+type PodsMetricSource struct {
+	// metricName is the name of the metric in question
+	MetricName string `json:"metricName"`
+	// targetAverageValue is the target value of the average of the
+	// metric across all relevant pods (as a quantity)
+	TargetAverageValue resource.Quantity `json:"targetAverageValue"`
+}
+
+// ResourceMetricSource indicates when to call webhook on a resource metric known to
+// Kubernetes, as specified in requests and limits, describing each pod in the
+// current target (e.g. CPU or memory).
+// The values will be averaged together before being compared to the target
+// +k8s:openapi-gen=true
+type ResourceMetricSource struct {
+	// name is the name of the resource in question.
+	Name v1.ResourceName `json:"name"`
+	// targetAverageUtilization is the target value of the average of the
+	// resource metric across all relevant pods, represented as a percentage of
+	// the requested value of the resource for the pods.
+	// +optional
+	TargetAverageUtilization *int32 `json:"targetAverageUtilization,omitempty"`
+	// targetAverageValue is the target value of the average of the
+	// resource metric across all relevant pods, as a raw value (instead of as
+	// a percentage of the request), similar to the "pods" metric source type.
+	// +optional
+	TargetAverageValue *resource.Quantity `json:"targetAverageValue,omitempty"`
 }
 
 // MetricWebhookStatus defines the observed state of MetricWebhook
 // +k8s:openapi-gen=true
 type MetricWebhookStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// lastScrapeTime is the last time the MetricWebhook scraped metrics
+	// +optional
+	LastScrapeTime *metav1.Time `json:"lastScrapeTime,omitempty"`
+	// currentMetrics is the last read state of the metrics used by this MetricWebhook.
+	// +listType=set
+	// +optional
+	CurrentMetrics []MetricStatus `json:"currentMetrics"`
+}
+
+// MetricStatus describes the last-read state of a single metric.
+// +k8s:openapi-gen=true
+type MetricStatus struct {
+	// type is the type of metric source.  It should be "Pods" or "Resource",
+	// each mapping to a matching field in the object.
+	Type MetricSourceType `json:"type"`
+	// pods refers to a metric describing each pod in the current target
+	// (for example, transactions-processed-per-second). The values will be
+	// averaged together before being compared to the target value.
+	// +optional
+	Pods *PodsMetricStatus `json:"pods,omitempty"`
+	// resource refers to a resource metric (such as those specified in
+	// requests and limits) known to Kubernetes describing each pod in the
+	// current target (e.g. CPU or memory). Such metrics are built in to
+	// Kubernetes, and have special scaling options on top of those available
+	// to normal per-pod metrics using the "pods" source.
+	// +optional
+	Resource *ResourceMetricStatus `json:"resource,omitempty"`
+}
+
+// PodsMetricStatus indicates the current value of a metric describing each pod in
+// the current scale target (for example, transactions-processed-per-second).
+// +k8s:openapi-gen=true
+type PodsMetricStatus struct {
+	// metricName is the name of the metric in question
+	MetricName string `json:"metricName"`
+	// currentAverageValue is the current value of the average of the
+	// metric across all relevant pods (as a quantity)
+	CurrentAverageValue resource.Quantity `json:"currentAverageValue"`
+}
+
+// ResourceMetricStatus indicates the current value of a resource metric known to
+// Kubernetes, as specified in requests and limits, describing each pod in the
+// current target (e.g. CPU or memory). Such metrics are built in to
+// Kubernetes, and have special scaling options on top of those available to
+// normal per-pod metrics using the "pods" source.
+// +k8s:openapi-gen=true
+type ResourceMetricStatus struct {
+	// name is the name of the resource in question.
+	Name v1.ResourceName `json:"name"`
+	// currentAverageUtilization is the current value of the average of the
+	// resource metric across all relevant pods, represented as a percentage of
+	// the requested value of the resource for the pods.  It will only be
+	// present if `targetAverageValue` was set in the corresponding metric
+	// specification.
+	// +optional
+	CurrentAverageUtilization *int32 `json:"currentAverageUtilization,omitempty"`
+	// currentAverageValue is the current value of the average of the
+	// resource metric across all relevant pods, as a raw value (instead of as
+	// a percentage of the request), similar to the "pods" metric source type.
+	// It will always be set, regardless of the corresponding metric specification.
+	CurrentAverageValue resource.Quantity `json:"currentAverageValue"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- pkg/apis/metrics/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/metrics/v1alpha1/zz_generated.openapi.go
@@ -5,15 +5,93 @@
 package v1alpha1
 
 import (
-	"github.com/go-openapi/spec"
-	"k8s.io/kube-openapi/pkg/common"
+	spec "github.com/go-openapi/spec"
+	common "k8s.io/kube-openapi/pkg/common"
 )
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"./pkg/apis/metrics/v1alpha1.MetricWebhook":       schema_pkg_apis_metrics_v1alpha1_MetricWebhook(ref),
-		"./pkg/apis/metrics/v1alpha1.MetricWebhookSpec":   schema_pkg_apis_metrics_v1alpha1_MetricWebhookSpec(ref),
-		"./pkg/apis/metrics/v1alpha1.MetricWebhookStatus": schema_pkg_apis_metrics_v1alpha1_MetricWebhookStatus(ref),
+		"./pkg/apis/metrics/v1alpha1.MetricSpec":           schema_pkg_apis_metrics_v1alpha1_MetricSpec(ref),
+		"./pkg/apis/metrics/v1alpha1.MetricStatus":         schema_pkg_apis_metrics_v1alpha1_MetricStatus(ref),
+		"./pkg/apis/metrics/v1alpha1.MetricWebhook":        schema_pkg_apis_metrics_v1alpha1_MetricWebhook(ref),
+		"./pkg/apis/metrics/v1alpha1.MetricWebhookSpec":    schema_pkg_apis_metrics_v1alpha1_MetricWebhookSpec(ref),
+		"./pkg/apis/metrics/v1alpha1.MetricWebhookStatus":  schema_pkg_apis_metrics_v1alpha1_MetricWebhookStatus(ref),
+		"./pkg/apis/metrics/v1alpha1.PodsMetricSource":     schema_pkg_apis_metrics_v1alpha1_PodsMetricSource(ref),
+		"./pkg/apis/metrics/v1alpha1.PodsMetricStatus":     schema_pkg_apis_metrics_v1alpha1_PodsMetricStatus(ref),
+		"./pkg/apis/metrics/v1alpha1.ResourceMetricSource": schema_pkg_apis_metrics_v1alpha1_ResourceMetricSource(ref),
+		"./pkg/apis/metrics/v1alpha1.ResourceMetricStatus": schema_pkg_apis_metrics_v1alpha1_ResourceMetricStatus(ref),
+		"./pkg/apis/metrics/v1alpha1.TargetRef":            schema_pkg_apis_metrics_v1alpha1_TargetRef(ref),
+		"./pkg/apis/metrics/v1alpha1.Webhook":              schema_pkg_apis_metrics_v1alpha1_Webhook(ref),
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_MetricSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "MetricSpec specifies metrics thresholds before webhook gets called",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"type": {
+						SchemaProps: spec.SchemaProps{
+							Description: "type is the type of metric source.  It should be \"Pods\" or \"Resource\", each mapping to a matching field in the object.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"pods": {
+						SchemaProps: spec.SchemaProps{
+							Description: "pods refers to a metric describing each pod in the current scale target (for example, transactions-processed-per-second). The values will be averaged together before being compared to the target value.",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.PodsMetricSource"),
+						},
+					},
+					"resource": {
+						SchemaProps: spec.SchemaProps{
+							Description: "resource refers to a resource metric (such as those specified in requests and limits) known to Kubernetes describing each pod in the current target (e.g. CPU or memory).",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.ResourceMetricSource"),
+						},
+					},
+				},
+				Required: []string{"type"},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/metrics/v1alpha1.PodsMetricSource", "./pkg/apis/metrics/v1alpha1.ResourceMetricSource"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_MetricStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "MetricStatus describes the last-read state of a single metric.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"type": {
+						SchemaProps: spec.SchemaProps{
+							Description: "type is the type of metric source.  It should be \"Pods\" or \"Resource\", each mapping to a matching field in the object.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"pods": {
+						SchemaProps: spec.SchemaProps{
+							Description: "pods refers to a metric describing each pod in the current target (for example, transactions-processed-per-second). The values will be averaged together before being compared to the target value.",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.PodsMetricStatus"),
+						},
+					},
+					"resource": {
+						SchemaProps: spec.SchemaProps{
+							Description: "resource refers to a resource metric (such as those specified in requests and limits) known to Kubernetes describing each pod in the current target (e.g. CPU or memory). Such metrics are built in to Kubernetes, and have special scaling options on top of those available to normal per-pod metrics using the \"pods\" source.",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.ResourceMetricStatus"),
+						},
+					},
+				},
+				Required: []string{"type"},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/metrics/v1alpha1.PodsMetricStatus", "./pkg/apis/metrics/v1alpha1.ResourceMetricStatus"},
 	}
 }
 
@@ -67,8 +145,49 @@ func schema_pkg_apis_metrics_v1alpha1_MetricWebhookSpec(ref common.ReferenceCall
 			SchemaProps: spec.SchemaProps{
 				Description: "MetricWebhookSpec defines the desired state of MetricWebhook",
 				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"targetRef": {
+						SchemaProps: spec.SchemaProps{
+							Description: "targetRef points to the target resource used to fetch pods for which metrics should be collected",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.TargetRef"),
+						},
+					},
+					"webhook": {
+						SchemaProps: spec.SchemaProps{
+							Description: "webhook points to the web endpoint that going to get metric alerts",
+							Ref:         ref("./pkg/apis/metrics/v1alpha1.Webhook"),
+						},
+					},
+					"metrics": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "set",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "metrics contains the specifications for metrics thresholds used to trigger webhook",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("./pkg/apis/metrics/v1alpha1.MetricSpec"),
+									},
+								},
+							},
+						},
+					},
+					"scrapeInterval": {
+						SchemaProps: spec.SchemaProps{
+							Description: "scrapeInterval defines how frequently to scrape metrics",
+							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Duration"),
+						},
+					},
+				},
+				Required: []string{"targetRef", "webhook", "metrics", "scrapeInterval"},
 			},
 		},
+		Dependencies: []string{
+			"./pkg/apis/metrics/v1alpha1.MetricSpec", "./pkg/apis/metrics/v1alpha1.TargetRef", "./pkg/apis/metrics/v1alpha1.Webhook", "k8s.io/apimachinery/pkg/apis/meta/v1.Duration"},
 	}
 }
 
@@ -78,6 +197,227 @@ func schema_pkg_apis_metrics_v1alpha1_MetricWebhookStatus(ref common.ReferenceCa
 			SchemaProps: spec.SchemaProps{
 				Description: "MetricWebhookStatus defines the observed state of MetricWebhook",
 				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"lastScrapeTime": {
+						SchemaProps: spec.SchemaProps{
+							Description: "lastScrapeTime is the last time the MetricWebhook scraped metrics",
+							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Time"),
+						},
+					},
+					"currentMetrics": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "set",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "currentMetrics is the last read state of the metrics used by this MetricWebhook.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("./pkg/apis/metrics/v1alpha1.MetricStatus"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/metrics/v1alpha1.MetricStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.Time"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_PodsMetricSource(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "PodsMetricSource indicates when to call webhook on a metric describing each pod in the current target (for example, transactions-processed-per-second). The values will be averaged together before being compared to the target value.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"metricName": {
+						SchemaProps: spec.SchemaProps{
+							Description: "metricName is the name of the metric in question",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"targetAverageValue": {
+						SchemaProps: spec.SchemaProps{
+							Description: "targetAverageValue is the target value of the average of the metric across all relevant pods (as a quantity)",
+							Ref:         ref("k8s.io/apimachinery/pkg/api/resource.Quantity"),
+						},
+					},
+				},
+				Required: []string{"metricName", "targetAverageValue"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/api/resource.Quantity"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_PodsMetricStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "PodsMetricStatus indicates the current value of a metric describing each pod in the current scale target (for example, transactions-processed-per-second).",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"metricName": {
+						SchemaProps: spec.SchemaProps{
+							Description: "metricName is the name of the metric in question",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"currentAverageValue": {
+						SchemaProps: spec.SchemaProps{
+							Description: "currentAverageValue is the current value of the average of the metric across all relevant pods (as a quantity)",
+							Ref:         ref("k8s.io/apimachinery/pkg/api/resource.Quantity"),
+						},
+					},
+				},
+				Required: []string{"metricName", "currentAverageValue"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/api/resource.Quantity"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_ResourceMetricSource(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "ResourceMetricSource indicates when to call webhook on a resource metric known to Kubernetes, as specified in requests and limits, describing each pod in the current target (e.g. CPU or memory). The values will be averaged together before being compared to the target",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"name": {
+						SchemaProps: spec.SchemaProps{
+							Description: "name is the name of the resource in question.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"targetAverageUtilization": {
+						SchemaProps: spec.SchemaProps{
+							Description: "targetAverageUtilization is the target value of the average of the resource metric across all relevant pods, represented as a percentage of the requested value of the resource for the pods.",
+							Type:        []string{"integer"},
+							Format:      "int32",
+						},
+					},
+					"targetAverageValue": {
+						SchemaProps: spec.SchemaProps{
+							Description: "targetAverageValue is the target value of the average of the resource metric across all relevant pods, as a raw value (instead of as a percentage of the request), similar to the \"pods\" metric source type.",
+							Ref:         ref("k8s.io/apimachinery/pkg/api/resource.Quantity"),
+						},
+					},
+				},
+				Required: []string{"name"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/api/resource.Quantity"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_ResourceMetricStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "ResourceMetricStatus indicates the current value of a resource metric known to Kubernetes, as specified in requests and limits, describing each pod in the current target (e.g. CPU or memory). Such metrics are built in to Kubernetes, and have special scaling options on top of those available to normal per-pod metrics using the \"pods\" source.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"name": {
+						SchemaProps: spec.SchemaProps{
+							Description: "name is the name of the resource in question.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"currentAverageUtilization": {
+						SchemaProps: spec.SchemaProps{
+							Description: "currentAverageUtilization is the current value of the average of the resource metric across all relevant pods, represented as a percentage of the requested value of the resource for the pods.  It will only be present if `targetAverageValue` was set in the corresponding metric specification.",
+							Type:        []string{"integer"},
+							Format:      "int32",
+						},
+					},
+					"currentAverageValue": {
+						SchemaProps: spec.SchemaProps{
+							Description: "currentAverageValue is the current value of the average of the resource metric across all relevant pods, as a raw value (instead of as a percentage of the request), similar to the \"pods\" metric source type. It will always be set, regardless of the corresponding metric specification.",
+							Ref:         ref("k8s.io/apimachinery/pkg/api/resource.Quantity"),
+						},
+					},
+				},
+				Required: []string{"name", "currentAverageValue"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/api/resource.Quantity"},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_TargetRef(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "TargetRef points to deployment or pod to watch metrics for",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind of the referent; More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds\"",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"name": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Name of the referent; More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+				Required: []string{"kind", "name"},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_metrics_v1alpha1_Webhook(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "Webhook describes the web endpoint that the operator calls on metrics reaching their thresholds",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"service": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind of the referent service",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"port": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Service port the webserver serves on",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"path": {
+						SchemaProps: spec.SchemaProps{
+							Description: "URL path to the webhook",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+				Required: []string{"service", "port", "path"},
 			},
 		},
 	}
